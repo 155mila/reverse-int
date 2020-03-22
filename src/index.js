d56f045c191c46c19270e21d806d75db7b5401a7
155mila <58401380+155mila@users.noreply.github.com>
2020-03-22 18:28:07 +0300

    find err

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function reverse (n) {
+    if(n < 0){
+        n = n * (-1)
+    }
     n += '';
     let newstr = n.split('').reverse().join('');
-    return Number(newstr);
+    return newstr;
 }
